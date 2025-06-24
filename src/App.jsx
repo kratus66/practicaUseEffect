838987ee1c40cf652864efb2d712647d01e1805a
Diego Herrera <diegoherrera1685@hotmail.com>
2025-06-23 21:37:04 -0500

    filtros y boton reset

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,68 @@ import Card from './Card'
 import Details from './Details';
 function App() {
   const[characters, setCharacters]=useState([])
-  const[detail, setDeteil]=useState(false);
+  const[detail, setDetail]=useState(false);
   const[id, setId]=useState(0);
+  const[casa, setCasa]=useState("");
+  const [filteredCharacters, setFilteredCharacters] = useState([]);
+  
+
 
   const handleOnClick=(id)=>{
     setId(id);
-    setDeteil(true)
+    setDetail(true)
 
     console.log("ID seleccionado:", id)
     console.log("¿Mostrar detalle?", detail)
 
   }
   const handleOnClose=()=>{
-    setDeteil(false);
+    setDetail(false);
   }
 
   useEffect(()=>{
     fetch('https://hp-api.onrender.com/api/characters')
     .then((response)=>response.json())
     .then((data)=>{
-      const primerosCinco=data.slice(0,5);
+      const primerosCinco=data.slice(2);
       setCharacters(primerosCinco)
+      setFilteredCharacters(primerosCinco)
     })
   }
   ,[])
 
+    const handleHouse = (e) => {
+    const casaSeleccionada = e.target.innerText;
+    setCasa(casaSeleccionada);
+
+    
+      const filtrados = characters.filter((char) => char.house === casaSeleccionada);
+      setFilteredCharacters(filtrados);
+      setRes(true);
+
+    
+  };
+
+   const handleReset=()=>{
+      setCasa("");
+      setFilteredCharacters(characters);
+
+   }
+
   return (
     <>
+      <button onClick={handleHouse}>Gryffindor</button>
+      <button onClick={handleHouse}>Slytherin</button>
+      <button onClick={handleHouse}>Hufflepuff</button>
+      <button onClick={handleHouse}>Ravenclaw</button>
+      <button onClick={handleReset}>Reestablecer</button>
     {
-      characters.map((character)=>{
+      filteredCharacters.map((character)=>{
         return(
-          <Card  key={character.id} character={character} handleOnClick={handleOnClick}/>
+          
+            
+            <Card  key={character.id} character={character} handleOnClick={handleOnClick}/>
+          
           
         )
       }
